e6fec9571c274ec1be7d48f12a8d130415941982
Rishabh Khatri <101245152+rishabhkhatri03@users.noreply.github.com>
2024-08-19 18:09:13 +0530

    A personal portfolio application

---
+++ Portfolio-main (1)/Portfolio-main/src/constants/index.js
@@ -0,0 +1,268 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    prepinsta,
+    oasis,
+    cisco,
+    ECELL,
+    pulse,
+    docochain,
+    millow,
+    indigo,
+    github,
+    flutter,
+    aws,
+    dart,
+    mysql,
+    postgre,
+    python,
+    heirloom4,
+    skyloom,
+    Proton
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Data Analyst",
+      icon: web,
+    },
+    {
+      title: "Python Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Cloud Computing",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Flutter",
+      icon: flutter,
+    },
+    {
+      name: "AWS",
+      icon: aws,
+    },
+    {
+      name: "Dart",
+      icon: dart,
+    },
+    {
+      name: "MySQL",
+      icon: mysql,
+    },
+    {
+      name: "PostgreSQL",
+      icon: postgre,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "github",
+      icon: github,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Android App Development-Intern",
+      company_name: "Oasis Infobyte",
+      icon: oasis,
+      iconBg: "#383E56",
+      date: "May 2023 - June 2023",
+      points: [
+        "Developed several Android application interfaces using Flutter and Dart for responsive layouts and seamless user experiences.",
+        "Implemented responsive design principles in Android apps, ensuring compatibility across different screen sizes and devices.",
+        "Executed three Android projects, showcasing my development process and technical insights through detailed LinkedIn blog posts."
+      ],
+    },
+    {
+      title: "Cloud Developer",
+      company_name: "Cisco-Aicte",
+      icon: cisco,
+      iconBg: "#E6DEDD",
+      date: "Year 2023",
+      points: [
+        "Participated in the Cisco Virtual Internship, gaining valuable hands-on experience and expanding my perspective in the dynamic field of cloud.",
+      ],
+    },
+    {
+      title: "Growth Manager",
+      company_name: "PrepInsta",
+      icon: prepinsta,
+      iconBg: "#383E56",
+      date: "2023-2024",
+      points: [
+        "As a Growth Manager at PrepInsta, involved in fundamental marketing activities, contributing to the promotion and visibility of the platform",
+        "Committed to learning and applying foundational marketing techniques to help PrepInsta reach a broader audience and provide valuable educational resources to its users.",
+      ],
+    },
+    {
+      title: "Core Lead",
+      company_name: "Team Indigo, GGITS",
+      icon: indigo,
+      iconBg: "#E6DEDD",
+      date: "2023 - Present",
+      points: [
+        "Spearheaded initiatives within the events and executives team at Team Indigo GGITS, aimed at fostering the interest of all the cultural events among students.",
+        "Led the planning and execution of various events with my team to showcase the best cultural spirits of our institute.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I’ve had the pleasure of working with Rishabh on multiple projects, and his ability to seamlessly collaborate, bring fresh ideas to the table, and drive results is truly commendable.",
+      name: "Abhiraj Tiwari",
+      designation: "Core Lead E-Cell",
+      company: "GGITS",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF5-3YjBcXTqKUlOAeUUtuOLKgQSma2wGG1g&s",
+    },
+    {
+      testimonial:
+        "Rishabh is a highly motivated and resourceful professional. His passion for technology and ability to find creative solutions to challenges make him a valuable asset.",
+      name: "Aditi Grover",
+      designation: "ASE",
+      company: "Carenol Global Solutions,Bangalore",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF5-3YjBcXTqKUlOAeUUtuOLKgQSma2wGG1g&s",
+    },
+    {
+      testimonial:
+        "Rishabh is an exceptional student with a keen interest in emerging technologies. His dedication, analytical mindset, and consistent performance make him stand out among his peers.",
+      name: "Siddharth Bhalerao",
+      designation: "Head Professor",
+      company: "GGITS,Jabalpur",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF5-3YjBcXTqKUlOAeUUtuOLKgQSma2wGG1g&s",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Proton",
+      description:
+      "Leading the development of an innovative generative AI project aimed at transforming daily online interactions for visually impaired individuals by using cutting-edge generative AI techniques and technologies",
+
+      tags: [
+        {
+          name: "python",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "sql",
+          color: "green-text-gradient",
+        },
+        {
+          name: "django",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: Proton,
+      source_code_link: "https://github.com/rishabhkhatri03/Proton.git",
+    },
+    {
+      name: "Heirloom",
+      description:
+      "An initiative designed to empower local artisans and grassroots workers by providing them a platform to showcase and sell their handmade products. The project focuses on promoting traditional craftsmanship.",
+      tags: [
+        {
+          name: "HTML/CSS/JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Python",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Django",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: heirloom4,
+      source_code_link: "https://github.com/rishabhkhatri03/Ecommerce-application.git",
+    },
+    {
+      name: "Skyloom",
+      description:
+        "A comprehensive weather detection application made with the use of Flutter and Dart which can give us the full details of the weather of any location with date and time as well.",
+      tags: [
+        {
+          name: "Flutter",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Dart",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Firebase",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: skyloom,
+      source_code_link: "https://github.com/rishabhkhatri03/SKYLOOM.git",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
